8925541a64dfef4ab3008c4890f140d92a7be43e
Thiago Pereira <thiago.spereira02@gmail.com>
2025-11-26 18:03:43 +0000

    chore: fix eslint node configuration and add npm scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  js.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.node, 
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    }
+  },
+];
